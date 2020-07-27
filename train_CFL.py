a02d99a369f65b1a536a07e0328dd64f0656f98f
palver7 <palver7@yahoo.com>
2020-07-27 19:57:38 +0700

    further unit test L2 regularization

--- train_CFL.py
+++ train_CFL.py
@@ -59,37 +59,41 @@ def evaluate(pred, gt):
     #gt = Image.open(gt_path_list[im])
     #gt = gt.resize([pred_W, pred_H])
     #gt = torch.tensor(gt)/255.
-    gt = (gt.ge(0.1)).int()
 
-    th=0.1
-    gtpos=gt.eq(1)
-    gtneg=gt.eq(0)
-    predgt=pred.gt(th)
-    predle=pred.le(th)
-    tp = torch.sum((gtpos & predgt).float())
-    tn = torch.sum((gtneg & predle).float())
-    fp = torch.sum((gtneg & predgt).float())
-    fn = torch.sum((gtpos & predle).float())
+    cumulativeIoU = 0.0
+    for i, batches in enumerate(zip(pred,gt)) :
+    
+        gt = (batches[1].ge(0.1)).int()
+        th=0.1
+        gtpos=gt.eq(1)
+        gtneg=gt.eq(0)
+        predgt=batches[0].gt(th)
+        predle=batches[0].le(th)
+        tp = torch.sum((gtpos & predgt).float())
+        tn = torch.sum((gtneg & predle).float())
+        fp = torch.sum((gtneg & predgt).float())
+        fn = torch.sum((gtpos & predle).float())
 
-    # How accurate the positive predictions are
-    #P.append(tp / (tp + fp))
-    P = tp / (tp + fp)
-    # Coverage of actual positive sample
-    #R.append(tp / (tp + fn))
-    R = (tp / (tp + fn))
-    # Overall performance of model
-    #Acc.append((tp + tn) / (tp + tn + fp + fn))
-    Acc = ((tp + tn) / (tp + tn + fp + fn))
-    # Hybrid metric useful for unbalanced classes 
-    #f1.append(2 * (tp / (tp + fp))*(tp / (tp + fn))/((tp / (tp + fp))+(tp / (tp + fn))))
-    f1 = (2 * (tp / (tp + fp))*(tp / (tp + fn))/((tp / (tp + fp))+(tp / (tp + fn))))
-    # Intersection over Union
-    #IoU.append(tp / (tp + fp + fn))
-    IoU = (tp / (tp + fp + fn))
-      
+        # How accurate the positive predictions are
+        #P.append(tp / (tp + fp))
+        #P = tp / (tp + fp)
+        # Coverage of actual positive sample
+        #R.append(tp / (tp + fn))
+        #R = (tp / (tp + fn))
+        # Overall performance of model
+        #Acc.append((tp + tn) / (tp + tn + fp + fn))
+        #Acc = ((tp + tn) / (tp + tn + fp + fn))
+        # Hybrid metric useful for unbalanced classes 
+        #f1.append(2 * (tp / (tp + fp))*(tp / (tp + fn))/((tp / (tp + fp))+(tp / (tp + fn))))
+        #f1 = (2 * (tp / (tp + fp))*(tp / (tp + fn))/((tp / (tp + fp))+(tp / (tp + fn))))
+        # Intersection over Union
+        #IoU.append(tp / (tp + fp + fn))
+        IoU = (tp / (tp + fp + fn))
+        cumulativeIoU += IoU.item() 
 
-    return torch.mean(P), torch.mean(R), torch.mean(Acc), torch.mean(f1), torch.mean(IoU)
+    #return torch.mean(P), torch.mean(R), torch.mean(Acc), torch.mean(f1), torch.mean(IoU)
     #return P, R, Acc, f1, IoU
+    return cumulativeIoU
 
 
 def ce_loss(pred, gt):
@@ -108,7 +112,7 @@ def ce_loss(pred, gt):
     pb = nb/total_pix
     ps = ns/total_pix
     
-    LogitsLoss= nn.BCEWithLogitsLoss()
+    LogitsLoss= nn.BCEWithLogitsLoss(reduction='none')
     ponderedSCELoss=LogitsLoss(pred,gt)
     pond = torch.mul(vs.permute(2,3,0,1),1/ps) + torch.mul(vb.permute(2,3,0,1),1/pb)
     pond = pond.permute(2,3,0,1)
@@ -176,7 +180,7 @@ class SUN360Dataset(Dataset):
         CM_name = self.images_data.iloc[idx, 2]
         CL_name = self.images_data.iloc[idx, 3]
         image = Image.open(img_name)
-        if image.mode !='RGB' :
+        if image.mode !='RGB':
             image = image.convert('RGB')
         EM = Image.open(EM_name)
         CM = Image.open(CM_name)
@@ -310,11 +314,14 @@ def map_predict(outputs, EM_gt,CM_gt):
     EM=F.interpolate(EM_gt,size=(output.shape[-2],output.shape[-1]),mode='bilinear',align_corners=True)
     CM=F.interpolate(CM_gt,size=(output.shape[-2],output.shape[-1]),mode='bilinear',align_corners=True)
     edges,corners =torch.chunk(output,2,dim=1)
-    P_e, R_e, Acc_e, f1_e, IoU_e = evaluate(edges,EM)
-    print('EDGES: IoU: ' + str('%.3f' % IoU_e) + '; Accuracy: ' + str('%.3f' % Acc_e) + '; Precision: ' + str('%.3f' % P_e) + '; Recall: ' + str('%.3f' % R_e) + '; f1 score: ' + str('%.3f' % f1_e))
-    P_c, R_c, Acc_c, f1_c, IoU_c = evaluate(corners, CM)
-    print('CORNERS: IoU: ' + str('%.3f' % IoU_c) + '; Accuracy: ' + str('%.3f' % Acc_c) + '; Precision: ' + str('%.3f' % P_c) + '; Recall: ' + str('%.3f' % R_c) + '; f1 score: ' + str('%.3f' % f1_c))
-
+    IoU_e = evaluate(edges,EM)
+    IoU_c = evaluate(corners, CM)
+    #P_e, R_e, Acc_e, f1_e, IoU_e = evaluate(edges,EM)
+    #print('EDGES: IoU: ' + str('%.3f' % IoU_e) + '; Accuracy: ' + str('%.3f' % Acc_e) + '; Precision: ' + str('%.3f' % P_e) + '; Recall: ' + str('%.3f' % R_e) + '; f1 score: ' + str('%.3f' % f1_e))
+    #P_c, R_c, Acc_c, f1_c, IoU_c = evaluate(corners, CM)
+    #print('CORNERS: IoU: ' + str('%.3f' % IoU_c) + '; Accuracy: ' + str('%.3f' % Acc_c) + '; Precision: ' + str('%.3f' % P_c) + '; Recall: ' + str('%.3f' % R_c) + '; f1 score: ' + str('%.3f' % f1_c))
+    
+    return IoU_e, IoU_c
 
 def _train(args):
     """
@@ -344,7 +351,7 @@ def _train(args):
     train_transform = transforms.Compose(
         [transforms.Resize((img_size[0],img_size[1])),
          transforms.ToTensor(),
-         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+         transforms.Normalize(mean=[0.485, 0.458, 0.408], std=[1.0, 1.0, 1.0])
         ])
     train_target_transform = transforms.Compose([transforms.Resize((img_size[0],img_size[1])),
                                            transforms.ToTensor()])
@@ -359,7 +366,7 @@ def _train(args):
                                        flip_gen,
                                        [mytransforms.RandomHorizontalFlip(flip_gen,p=0.5),mytransforms.RandomHorizontalFlip(flip_gen,p=0.5),mytransforms.RandomHorizontalFlip(flip_gen,p=0.5), None],
                                        [transforms.ToTensor(),transforms.ToTensor(),transforms.ToTensor(), None],
-                                       [transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), None, None, None],
+                                       [transforms.Normalize(mean=[0.485, 0.458, 0.408], std=[1.0, 1.0, 1.0]), None, None, None],
                                        roll_gen,
                                        [mytransforms.RandomHorizontalRoll(roll_gen,p=0.5),mytransforms.RandomHorizontalRoll(roll_gen,p=0.5),mytransforms.RandomHorizontalRoll(roll_gen,p=0.5),None],
                                        [transforms.RandomErasing(p=0.5,scale=(0.01,0.02),ratio=(0.3,3.3),value=0), None, None, None],
@@ -368,7 +375,7 @@ def _train(args):
     valid_transform = transforms.Compose(
         [transforms.Resize((img_size[0],img_size[1])),
          transforms.ToTensor(),
-         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+         transforms.Normalize(mean=[0.485, 0.458, 0.408], std=[1.0, 1.0, 1.0])])
     valid_target_transform = transforms.Compose([transforms.Resize((img_size[0],img_size[1])),
                                            transforms.ToTensor()])     
 
@@ -382,7 +389,7 @@ def _train(args):
     valid = Subset(trainvalidset, valid_idx)
     trainset = SplitDataset(train, transform = None, target_transform = None, joint_transform=train_joint_transform)
     """
-    trainset = SUN360Dataset(file="traindata.json",transform = train_transform, target_transform = train_target_transform, joint_transform=None)
+    trainset = SUN360Dataset(file="traindata.json",transform = None, target_transform = None, joint_transform=train_joint_transform)
     train_loader = DataLoader(trainset, batch_size=args.batch_size,
                                                shuffle=True, num_workers=args.workers)
     
@@ -408,7 +415,6 @@ def _train(args):
     else:    
         model = model_fn(args.model_dir,args.model_name, args.conv_type, args.modelfile)
         print("resuming from a saved model")   
-    """for freezing encoders"""    
     #ct = 0
     #for child in model.children():
     #    ct+=1
@@ -421,13 +427,15 @@ def _train(args):
     WDecay = 5e-4
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr,weight_decay=0)
     LR_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer,0.995)
-    writer= SummaryWriter(log_dir="{}".format(args.logdir),comment="visualising losses of training and validation")
+    writer= SummaryWriter(log_dir="{}".format(args.logdir),comment="testing complete traindatastruct")
 
     for epoch in progressbar(range(1, args.epochs+1),redirect_stdout=True):
         epochtime1=time.time()
         # training phase
         phase = 'train'
         running_loss = 0.0
+        running_IoU_e = 0.0
+        running_IoU_c = 0.0
         for i, data in enumerate(train_loader):
             # get the inputs
             inputs, EM , CM = data
@@ -460,27 +468,36 @@ def _train(args):
                 convert_to_images(outputs,epoch,phase)
             EMLoss, CMLoss = map_loss(outputs,EM,CM,criterion)
             #loss = EMLoss + CMLoss
-            loss = EMLoss + CMLoss + WDecay * 0.5 * l2_reg
+            loss = EMLoss + CMLoss + WDecay * 0.5 * (l2_reg / inputs.size(0))
+            IoU_e, IoU_c = map_predict(outputs,EM,CM)
             loss.backward()
             optimizer.step()
 
             # print statistics
-            running_loss += loss.item()
+            running_loss += loss.item() * inputs.size(0)
+            running_IoU_e += IoU_e
+            running_IoU_c += IoU_c
             """
             if i % 1 == 0:  # print every 1 mini-batches
                 print('[%d, %5d] loss: %.3f' %
                       (epoch, i + 1, running_loss / args.batch_size))
                 running_loss = 0.0
             """
-        epoch_loss = running_loss / len(trainset)   
-        print("epoch: {}".format(epoch),", train_loss: %.3f" %(epoch_loss))
+        epoch_loss = running_loss / len(trainset) 
+        epoch_edge_IoU = running_IoU_e / len(trainset)
+        epoch_corner_IoU = running_IoU_c / len(trainset)  
+        print("epoch: {}".format(epoch),", training_loss: %.3f" %(epoch_loss))
         writer.add_scalar("training_loss", epoch_loss,epoch)
+        writer.add_scalar("training_edge_IoU", epoch_edge_IoU,epoch)
+        writer.add_scalar("training_corner_IoU", epoch_corner_IoU,epoch)
     
         # validation phase
         if(epoch%1==0):
             phase = 'val'
             with torch.no_grad():
                 running_loss = 0.0
+                running_IoU_e = 0.0
+                running_IoU_c = 0.0
                 for i, data in enumerate(valid_loader):
                     # get the inputs
                     inputs, EM , CM = data
@@ -500,16 +517,23 @@ def _train(args):
                         convert_to_images(outputs,epoch,phase)
                     EMLoss, CMLoss = map_loss(outputs,EM,CM,criterion)
                     #loss = EMLoss + CMLoss
-                    loss = EMLoss + CMLoss + WDecay * 0.5 * l2_reg
+                    loss = EMLoss + CMLoss + WDecay * 0.5 * (l2_reg / inputs.size(0))
+                    IoU_e, IoU_c = map_predict(outputs,EM,CM)
                     # print statistics
-                    running_loss += loss.item()
-                    #map_predict(outputs,EM,CM)
+                    running_loss += loss.item() * inputs.size(0)
+                    running_IoU_e += IoU_e
+                    running_IoU_c += IoU_c
+                
                       
-                epoch_loss = running_loss / len(validset)    
-                print("epoch: {}".format(epoch),", valid_loss: %.3f" %(epoch_loss))
-                writer.add_scalar("validation loss",epoch_loss,epoch)
-        if (epoch%100==0):
-            _save_model(model, args.model_dir, epoch)        
+                epoch_loss = running_loss / len(validset) 
+                epoch_edge_IoU = running_IoU_e / len(validset)
+                epoch_corner_IoU = running_IoU_c / len(validset)  
+                print("epoch: {}".format(epoch),", validation_loss: %.3f" %(epoch_loss))
+                writer.add_scalar("validation_loss", epoch_loss,epoch)
+                writer.add_scalar("validation_edge_IoU", epoch_edge_IoU,epoch)
+                writer.add_scalar("validation_corner_IoU", epoch_corner_IoU,epoch)
+        if (epoch%100==0 or epoch==args.epochs):
+            _save_model(model, args.model_dir, args.model_name ,epoch)        
         LR_scheduler.step()  
         epochtime2 = time.time()
     epochdiff = epochtime2 - epochtime1          
@@ -519,9 +543,9 @@ def _train(args):
     
 
 
-def _save_model(model, model_dir, epoch):
+def _save_model(model, model_dir, model_name, epoch):
     logger.info("Saving the model.")
-    modelfile = "model_epoch"+str(epoch)+".pth"
+    modelfile = "model_{}_epoch".format(model_name)+str(epoch)+".pth"
     path = os.path.join(model_dir, modelfile)
     # recommended way from http://pytorch.org/docs/master/notes/serialization.html
     torch.save(model.cpu().state_dict(), path)
@@ -560,8 +584,8 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, default=2.5e-4, metavar='LR',
                         help='initial learning rate (default: 2.5e-4)')
     parser.add_argument('--momentum', type=float, default=0.9, metavar='M', help='momentum (default: 0.9)')
-    parser.add_argument('--model-dir', type=str, default="")
-    parser.add_argument('--model-name', type=str,default="efficientnet-b0")
+    parser.add_argument('--model_dir', type=str, default="")
+    parser.add_argument('--model_name', type=str,default="ResNet50")
     parser.add_argument('--conv_type', type=str,default="Std", help='select convolution type between Std and Equi. Also determines the network type')
     parser.add_argument('--logdir', type=str,default="", help='save directory for tensorboard event files')
     parser.add_argument('--modelfile', type=str, default=None, help="load model file for resuming training")
