4310bb17b3f83bd6e059a28aa418b02ff78951f8
palver7 <palver7@yahoo.com>
2020-08-14 21:20:58 +0700

    adding bias to conv1 after rgb_input

--- CFLPytorch/resnet.py
+++ CFLPytorch/resnet.py
@@ -145,7 +145,7 @@ class ResNet(nn.Module):
         self.groups = groups
         self.base_width = width_per_group
         self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
-                               bias=False)
+                               bias=True)
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
