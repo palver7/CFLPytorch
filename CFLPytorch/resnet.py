f8dd82d91da149b451e920f0f68d3baed944fb67
palver7 <palver7@yahoo.com>
2020-08-17 18:00:59 +0700

    setting inplace flags to False

--- CFLPytorch/resnet.py
+++ CFLPytorch/resnet.py
@@ -48,7 +48,7 @@ class BasicBlock(nn.Module):
         # Both self.conv1 and self.downsample layers downsample the input when stride != 1
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = norm_layer(planes)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace=False)
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = norm_layer(planes)
         self.downsample = downsample
@@ -91,8 +91,8 @@ class Bottleneck(nn.Module):
         self.bn2 = norm_layer(width)
         self.conv3 = conv1x1(width, planes * self.expansion)
         self.bn3 = norm_layer(planes * self.expansion)
-        self.dropout = nn.Dropout(1-0.5, inplace=True)
-        self.relu = nn.ReLU(inplace=True)
+        self.dropout = nn.Dropout(1-0.5, inplace=False)
+        self.relu = nn.ReLU(inplace=False)
         self.downsample = downsample
         self.stride = stride
 
@@ -147,7 +147,7 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3,
                                bias=True)
         self.bn1 = norm_layer(self.inplanes)
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace=False)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0],layer_index=1)
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
