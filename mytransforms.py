bc8cec70ff08870782d7787245b08100d92a5704
palver7 <palver7@yahoo.com>
2020-08-25 21:00:41 +0700

    adding separate panostretch for corners

--- mytransforms.py
+++ mytransforms.py
@@ -22,6 +22,55 @@ def _iterate_transforms(transforms, x):
 # the first level will be applied to all inputs
 # and nested levels are passed to nested transforms
 
+def gaussian_radius(det_size, min_overlap=0.7):
+  height, width = det_size
+
+  a1  = 1
+  b1  = (height + width)
+  c1  = width * height * (1 - min_overlap) / (1 + min_overlap)
+  sq1 = np.sqrt(b1 ** 2 - 4 * a1 * c1)
+  r1  = (b1 + sq1) / 2
+
+  a2  = 4
+  b2  = 2 * (height + width)
+  c2  = (1 - min_overlap) * width * height
+  sq2 = np.sqrt(b2 ** 2 - 4 * a2 * c2)
+  r2  = (b2 + sq2) / 2
+
+  a3  = 4 * min_overlap
+  b3  = -2 * min_overlap * (height + width)
+  c3  = (min_overlap - 1) * width * height
+  sq3 = np.sqrt(b3 ** 2 - 4 * a3 * c3)
+  r3  = (b3 + sq3) / 2
+  return min(r1, r2, r3)
+
+
+def gaussian2D(shape, sigma=1):
+    m, n = [(ss - 1.) / 2. for ss in shape]
+    y, x = np.ogrid[-m:m+1,-n:n+1]
+
+    h = np.exp(-(x * x + y * y) / (2 * sigma * sigma))
+    h[h < np.finfo(h.dtype).eps * h.max()] = 0
+    return h
+
+def draw_umich_gaussian(heatmap, center, radius=25, k=1):
+  diameter = 2 * radius + 1
+  gaussian = gaussian2D((diameter, diameter), sigma=diameter / 6)
+  
+  #x, y = int(center[0]), int(center[1])
+  for x,y in center:  
+    x, y = int(np.rint(x)), int(np.rint(y))
+    height, width = heatmap.shape[0:2]
+
+    left, right = min(x, radius), min(width - x, radius + 1)
+    top, bottom = min(y, radius), min(height - y, radius + 1)
+
+    masked_heatmap  = heatmap[y - top:y + bottom, x - left:x + right]
+    masked_gaussian = gaussian[radius - top:radius + bottom, radius - left:radius + right]
+    if min(masked_gaussian.shape) > 0 and min(masked_heatmap.shape) > 0: # TODO debug
+        np.maximum(masked_heatmap, masked_gaussian * k, out=masked_heatmap)
+  return heatmap
+
 class Compose(object):
     def __init__(self, transforms):
         self.transforms = transforms
@@ -187,6 +236,23 @@ class RandomPanoStretch(object):
             image = Image.fromarray(image)
             if image.mode !='RGB':
                 image=image.convert('L')
+        
+        return image
+
+class RandomPanoStretchCorners(object):
+    def __init__(self, gen, p=0.5):
+        self.p = p
+        self._gen = gen
+    
+    def __call__(self, image):
+        if self._gen.apply < self.p:
+            image = np.asarray(image)
+            if image.ndim < 3:
+                image = np.expand_dims(image,axis=-1)    
+            image, cor = panostretch.pano_stretch(image, self._gen.cor, self._gen.kx, self._gen.ky)
+            hm = np.zeros((512, 1024), dtype=np.float32)
+            hm = (draw_umich_gaussian(hm,cor)*255).astype(np.uint8)
+            CM = Image.fromarray(hm)    
 
 
-        return image       
+        return CM              
